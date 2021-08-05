14c5499ff53690dafc3ea64a7740026d68b77e97
Fayçal Arbai <faycala@graphcore.ai>
2021-08-05 11:48:10 +0100

    upgraded version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ docs_requires = all_requires + ["sphinx", "sphinxbootstrap4theme"]
 with open("README.md", "r") as readme:
     setup(
         name='resilient-exporters',
-        version='0.1.3',
+        version='0.1.4',
         packages=['resilient_exporters'],
         python_requires='>=3.6',
         setup_requires=["setuptools", "wheel"],
